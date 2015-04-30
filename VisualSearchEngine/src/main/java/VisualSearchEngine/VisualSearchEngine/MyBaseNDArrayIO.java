2eacd744e2576776b20e8de7a88c16711f3b8dc2
Changhyun Lee <chlee021690@gmail.com>
2015-04-30 16:05:49 -0400

    included the image-processing codebase

---
+++ VisualSearchEngine/src/main/java/VisualSearchEngine/VisualSearchEngine/MyBaseNDArrayIO.java
@@ -0,0 +1,15 @@
+package VisualSearchEngine.VisualSearchEngine;
+
+import org.nd4j.linalg.api.io.BaseNDArrayIO;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import java.io.File;
+import java.io.IOException;
+/*
+class MyBaseNDArrayIO extends BaseNDArrayIO {
+	
+	public MyBaseNDArrayIO() {}
+	
+	
+	
+}
+*/
